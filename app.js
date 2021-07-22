57435b49d4a2a809d10a2a8c19d8a29229a34c55
Xz3ra <47650386+Mathieu52@users.noreply.github.com>
2021-07-21 20:33:32 -0400

    Fire version

--- app.js
+++ app.js
@@ -3,25 +3,26 @@ var constraints = { video: { facingMode: "environment" }, audio: false };
 var id;
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
-      cameraSensor = document.querySelector("#camera--sensor"),
-      mapPointer = document.getElementById("map-pointer"),
-      text2 = document.getElementById("text")
+    cameraSensor = document.querySelector("#camera--sensor"),
+	mapPointer = document.getElementById("map-pointer")
+	text2 = document.getElementById("text")
 // Access the device camera and stream to cameraView
 function cameraStart() {
-	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-		track = stream.getTracks()[0];
-		cameraView.srcObject = stream;
-	}).catch(function(error) {
-		console.error("Oops. Something is broken.", error);
-	});
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
 }
 
 function updatePosition() {
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(showPosition, error);
 	}
-	text2.innerHTML = "show"+Math.random();
-	//text2.innerHTML = Math.random();
 	id = setTimeout(updatePosition, 10);
 }
 
@@ -29,12 +30,12 @@ function error(err) {
 	text2.innerHTML = `ERREUR (${err.code}): ${err.message}`;
 }
 
-function showPosition(pos) {
-	//text2.innerHTML = "show"+Math.random();
+function showPosition(position) {
+	text2.innerHTML = Math.random();
+	//text2.innerHTML = position.coords.latitude;
 	//mapPointer.style.left = position.coords.latitude*5+"px";
-	//bmapPointer.style.top = position.coords.longitude*5+"px";
+	//mapPointer.style.top = position.coords.longitude*5+"px";
 }
 
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
-
