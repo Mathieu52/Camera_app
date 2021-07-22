97dcce1b88f0030b109fbab7e1b33c5e272a8cf8
Xz3ra <47650386+Mathieu52@users.noreply.github.com>
2021-07-22 12:38:06 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // Set constraints for the video stream
 var constraints = { video: { facingMode: "environment" }, audio: false };
 var id;
+var dx = 5;
+var dy = 5;
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
     cameraSensor = document.querySelector("#camera--sensor"),
-	mapPointer = document.getElementById("map-pointer")
+    mapPointer = document.getElementById("map-pointer")
 	text2 = document.getElementById("text")
 // Access the device camera and stream to cameraView
 function cameraStart() {
+    getAccel();
     navigator.mediaDevices
         .getUserMedia(constraints)
         .then(function(stream) {
@@ -31,10 +34,47 @@ function error(err) {
 }
 
 function showPosition(position) {
-	text2.innerHTML = "show"+position.coords.latitude;
-	//text2.innerHTML = position.coords.latitude;
-	mapPointer.style.left = position.coords.latitude*5+"px";
-	mapPointer.style.top = position.coords.longitude*5+"px";
+    //text2.innerHTML = "show" + window.innerWidth;
+
+    const pointerHeight = 5;
+    var a = 0;
+    dx -= 0.01;
+    dy-= 0.01;
+    //var dx = 5;
+    //var dy = 5;
+    var d = Math.sqrt(Math.pow(dx, 2) + Math.pow(dx, 2)) * 0.05;
+    var A = Math.atan2(dy, dx);
+    //text2.innerHTML = pointerHeight / d;
+
+    mapPointer.style.left = (Math.cos(a * Math.PI / 180 + A) * window.innerWidth / 2 + window.innerWidth / 2 - mapPointer.offsetWidth / 2).toString() + "px";
+    mapPointer.height = (pointerHeight / d).toString();
+    mapPointer.width = (pointerHeight / d).toString();
+
+    mapPointer.style.display = Math.cos(a * Math.PI / 180 - A) > 0 ? "block" : "none";
+    //mapPointer.style.left = (window.innerWidth / 2).toString() + "px";
+    //mapPointer.style.left = Math.sin(a * Math.PI / 180) * dx + Math.cos(a * Math.PI / 180) * dy;
+    //mapPointer.style.top
+    //text2.innerHTML = position.coords.latitude;
+    //mapPointer
+	//mapPointer.style.left = position.coords.latitude*5+"px";
+	//mapPointer.style.top = position.coords.longitude*5+"px";
+}
+
+function getAccel() {
+    DeviceMotionEvent.requestPermission().then(response => {
+        if (response == 'granted') {
+            // Add a listener to get smartphone acceleration 
+            // in the XYZ axes (units in m/s^2)
+            window.addEventListener('devicemotion', (event) => {
+                text2.innerHTML = event.alpha;
+            });
+            // Add a listener to get smartphone orientation 
+            // in the alpha-beta-gamma axes (units in degrees)
+            window.addEventListener('deviceorientation', (event) => {
+                console.log(event);
+            });
+        }
+    });
 }
 
 // Start the video stream when the window loads
